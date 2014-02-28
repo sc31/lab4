b4b8fe539e95ef8e2bbb3c7d10f397adfc5c9ba1
Aisha F <aisha@cs.brown.edu>
2014-02-28 17:14:00 -0500

    insertion sort

--- Sorting/src/Sorting/Main.java
+++ Sorting/src/Sorting/Main.java
@@ -22,6 +22,11 @@ public class Main {
 		
 		System.out.println("Sorted Array:");
 		//TODO: sort the array and print out its contents
+    int[] sortedArray = InsertionSort.insertionSort(unsortedArray);
+    for (int i: sortedArray)
+				System.out.print(i + " ");
+		}
+		System.out.println();
 		
 		
 	}
